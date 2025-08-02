3fcd4a309ffcf8e4ebf578150cbceef50468de1b
Srijan Sinha <sinhasrijan730@gmail.com>
2025-08-03 00:03:45 +0530

    update: changed serverUrl to production backend

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,7 +5,9 @@ export const AuthDataContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [authData, setAuthData] = useState(null);
-  const serverUrl = "http://localhost:8080/"; 
+ // const serverUrl = "http://localhost:8080/"; 
+  const serverUrl = "https://citycart-backend-60ob.onrender.com/";
+
 
   return (
     <AuthDataContext.Provider value={{ authData, setAuthData, serverUrl }}>
